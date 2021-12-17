771b96dc773cfbc107bb1945d280402af607e915
Anton Khlebnikov <toni.farsh@icloud.com>
2021-12-17 10:31:22 +0500

    add real

--- src/main/java/CreditCalc.java
+++ src/main/java/CreditCalc.java
@@ -1,13 +1,19 @@
 public class CreditCalc {
     public double calculationOfMonthlyPayment(double creditAmount, double interestRate, double numberOfPayments) {
-
+        final double TOTALPERCENT = 100;
+        final double MONTHSOFJEAR = 12;
+        double monthlyInterestRate = interestRate / MONTHSOFJEAR / TOTALPERCENT;
+        return (creditAmount * (monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numberOfPayments))) /
+                ((Math.pow((1 + monthlyInterestRate), numberOfPayments)) - 1));
     }
 
     public double calculationTotalRefundAmount(double creditAmount, double interestRate, double numberOfPayments) {
-
+        double monthlyPayment = calculationOfMonthlyPayment(creditAmount, interestRate, numberOfPayments);
+        return monthlyPayment * numberOfPayments;
     }
 
     public  double calculationOverpayment(double creditAmount, double interestRate, double numberOfPayments) {
-
+        double totalRefundAmount = calculationTotalRefundAmount(creditAmount, interestRate, numberOfPayments);
+        return totalRefundAmount - creditAmount;
     }
 }
